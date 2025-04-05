212599f343b38aeb8f1e5dc67f160b7b73431cdf
fowleroc <=>
2025-04-05 14:05:23 -0400

    modified:   linked_list.c 	modified:   queue.c

--- linked_list.c
+++ linked_list.c
@@ -4,15 +4,60 @@
 
 struct list_node *new_node(size_t value) { return NULL; }
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *new = malloc(sizeof(struct list_node));
+  new->value = value;
+  new->next = list->head;
+  list->head = new;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node *new = malloc(sizeof(struct list_node));
+  new->value = value;
+  new->next = NULL;
+  if (list->head == NULL){
+    list->head == new;
+  }
+  else{
+    struct list_node *curr = list->head;
+    while(curr->next != NULL){
+      curr = curr->next;
+    }
+    curr->next = new;
+  }
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) { 
+  struct list_node *unwanted = list->head;
+  list->head = unwanted->next;
+  return 0; 
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list) { 
+  if (list->head->next == NULL){
+    list->head = NULL;
+  }
+  else{
+    struct list_node *curr = list->head;
+    while(curr->next !=NULL){
+      curr = curr->next;
+    }
+    curr->next = NULL;
+  }
+  return 0; 
+}
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list) {
+  struct list_node *curr = list.head;
+  while(curr !=NULL){
+    struct list_node *nextNode = curr->next;
+    free(curr);
+    curr = nextNode;
+  }
+  list.head = NULL;
+  free(&list);
+}
+
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
